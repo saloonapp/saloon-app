24bee8c0f8a339fddb872a10b6d96a953955717a
Quentin Valmori <quentin.valmori@gmail.com>
2015-03-16 23:58:59 +0100

    Working... Just have to check why I get so much console.log

--- www/js/poll/services.js
+++ www/js/poll/services.js
@@ -19,7 +19,6 @@ angular.module('app')
     function getPollsAround(){
       var matchDistance = 0.1; // 100m
       var matchMaxAge = 60 * 60 * 1000; // 1h
-      var pollsToReturn = [];
 
       return UserSrv.getCurrent().then(function(user){
     	  return pollCrud.find({
@@ -31,23 +30,7 @@ angular.module('app')
 	            $maxDistanceInKilometers: matchDistance
 	          }
 	        }).then(function(polls){
-          var deferred = $q.defer();
-          var promises = [];
-            _.map(polls, function(poll){
-              //Maybe we need a Poll Model in order to be sure to have the right informations every time
-              //and to have a cleaner code?
-              //var pollFormated = {};
-              //pollFormated = ;
-
-              promises.push(getAnswers(poll.objectId).then(function(result){
-                pollsToReturn.push(angular.extend({poll : poll},result));
-              }));
-            });
-          $q.all(promises).then(function(){
-            deferred.resolve(pollsToReturn);
-            //return pollsToReturn;
-          });
-          return deferred.promise;
+            return polls;
 	        });
 
     	})
@@ -56,76 +39,55 @@ angular.module('app')
     function initFirebase(){
 
     }
-    function getAnswers(pollid){
+    function getAnwsersForPolls(polls){
+      var deferred = $q.defer();
+      var promises = [];
+      var pollsToReturn = [];
+
+      _.map(polls, function(poll){
+        //Maybe we need a Poll Model in order to be sure to have the right informations every time
+        //and to have a cleaner code?
+        //var pollFormated = {};
+        //pollFormated = ;
+
+        promises.push(getAnswers(poll).then(function(result){
+          pollsToReturn.push(angular.extend(poll,result));
+        }));
+      });
+      $q.all(promises).then(function(){
+        deferred.resolve(pollsToReturn);
+        //return pollsToReturn;
+      });
+      return deferred.promise;
+    }
+    function getAnswers(poll){
       var answers;
-      var answersPath = getAnswersPath(pollid);
-      var statsPath = getStatsPath(pollid);
+      var answersPath = getAnswersPath(poll.objectId);
+      var statsPath = getStatsPath(poll.objectId);
       var defer = $q.defer();
-      answers = $firebaseArray(answersPath);
+
       var answersStats = null;
-        answers.$loaded().then(function(answers){
+      $firebaseArray(answersPath).$loaded().then(function(result){
+        console.log("result : ", result);
+        answers = result;
         $firebaseArray(statsPath).$loaded().then(function(stats){
+          console.log("stats : ", stats);
           answersStats = stats;
-          var toReturn = {
+          var toReturn = angular.extend({
             answers : answers,
             answersStats: answersStats,
-            pollid : pollid
-          };
-          defer.resolve(toReturn);
-
-
+            pollid : poll.objectId
+          }, poll);
+          defer.resolve(structurePolls(toReturn));
         });
       });
-
       return defer.promise;
-
-
     }
 
-    function saveAnswer(poll, choices, user){
 
-      var answers = $firebaseArray(getAnswersPath(poll.poll.objectId));
-      var statsPath = getStatsPath(poll.poll.objectId);
-      var answersStats = $firebaseArray(statsPath);
 
-      _.map(choices, function(choice){
-        answers.$add({
-          choice : choice.id,
-          user : user.objectId
-        });
-
-        var statsChoice = statsPath.child(choice.id);
-        statsChoice.transaction(function(current_val){
-          if(!current_val){
-            current_val = 0;
-          }
-          current_val++;
-          return current_val;
-        });
-        var totalVotes = statsPath.child('totalVotes');
-        totalVotes.transaction(function(current_val){
-          if(!current_val){
-            current_val = 0;
-          }
-          current_val++;
-          return current_val;
-        });
-      });
-      poll.poll.answers = answers;
-      poll.poll.answersStats = answersStats;
-      return {
-        answers : answers,
-        answersStats: answersStats,
-        pollid : poll.poll.objectId,
-        poll : structurePolls([poll])[0]
-
-      };
-    }
-
-    function structurePolls(polls){
-      var structuredPolls = [];
-      _.map(polls, function(poll){
-        _.map(poll.poll.choices, function(choice){
+    function structurePolls(poll){
+    /*    _.map(poll.choices, function(choice){
           var index = poll.answersStats[_.findIndex(poll.answersStats, function(chr){ return chr.$id == choice.id})];
           if( index === undefined){
             index = {};
@@ -139,10 +101,46 @@ angular.module('app')
           totalVotes.$value = 0;
         }
         poll.totalVotes = totalVotes.$value;
-      });
-      return polls;
+    */
+      return poll;
     }
+    function saveAnswer(poll, choices, user){
 
+      //var answers = $firebaseArray(getAnswersPath(poll.objectId));
+      var statsPath = getStatsPath(poll.objectId);
+      //var answersStats = $firebaseArray(statsPath);
+      var deferred = $q.defer();
+      _.map(choices, function(choice){
+        poll.answers.$add({
+          choice : choice.id,
+          user : user.objectId
+        });
+
+        var statsChoice = statsPath.child(choice.id);
+        statsChoice.transaction(function(current_val){
+          if(!current_val){
+            current_val = 0;
+          }
+          current_val++;
+          return current_val;
+        }, function(){
+          var totalVotes = statsPath.child('totalVotes');
+          totalVotes.transaction(function(current_val){
+            if(!current_val){
+              current_val = 0;
+            }
+            current_val++;
+            return current_val;
+          }, function(){
+            poll.pollid = poll.objectId;
+            poll = structurePolls(poll);
+            console.log('save answers', poll);
+            deferred.resolve(poll);
+          });
+        });
+      });
+      return deferred.promise;
+}
 /*
 maybe use some or every. The use of break can be good for a performance point of view...
  */
@@ -174,7 +172,8 @@ maybe use some or every. The use of break can be good for a performance point of
     saveAnswer : saveAnswer,
     getAnswers : getAnswers,
     hasUserAlreadyVoted : hasUserAlreadyVoted,
-    structurePolls : structurePolls
+    structurePolls : structurePolls,
+    getAnwsersForPolls : getAnwsersForPolls
   };
 
 
