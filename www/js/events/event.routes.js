b345268bd8352e0554af3171b6c6e6b5918ee05b
Loïc Knuchel <loicknuchel@gmail.com>
2015-05-16 15:33:03 +0200

    refactor event routes

--- www/js/events/event.routes.js
+++ www/js/events/event.routes.js
@@ -4,54 +4,102 @@
     .config(configure);
 
   function configure($stateProvider){
-    $stateProvider
-      .state('app.events', {
-      url: '/events',
-      templateUrl: 'js/events/events.html',
-      controller: 'EventsCtrl',
-      resolve: {
-        events: function(EventSrv, IonicUtils){
-          return IonicUtils.withLoading(EventSrv.getAll());
+    var routes = {
+      events: {
+        url: '/events',
+        templateUrl: 'js/events/events.html',
+        controller: 'EventsCtrl',
+        resolve: {
+          events: function(EventSrv, IonicUtils){
+            return IonicUtils.withLoading(EventSrv.getAll());
+          }
         }
-      }
-    })
-      .state('app.event', {
-      url: '/events/:eventId',
-      abstract: true,
-      cache: false, // see http://forum.ionicframework.com/t/problem-with-tabs-in-detail-view/22651
-      templateUrl: 'js/events/event.html',
-      controller: 'EventCtrl',
-      resolve: {
-        event: function($stateParams, EventSrv, IonicUtils){
-          return IonicUtils.withLoading(EventSrv.get($stateParams.eventId));
+      },
+      event: {
+        url: '/events/:eventId',
+        abstract: true,
+        cache: false, // see http://forum.ionicframework.com/t/problem-with-tabs-in-detail-view/22651
+        templateUrl: 'js/events/event.html',
+        controller: 'EventCtrl',
+        resolve: {
+          event: function($stateParams, EventSrv, IonicUtils){
+            return IonicUtils.withLoading(EventSrv.get($stateParams.eventId));
+          },
+          userData: function($stateParams, EventSrv, IonicUtils){
+            return IonicUtils.withLoading(EventSrv.getUserData($stateParams.eventId));
+          }
+        }
+      },
+      eventInfo: {
+        url: '/infos',
+        views: {
+          'infos-tab': {
+            templateUrl: 'js/events/event-infos.html',
+            controller: 'EventInfosCtrl'
+          }
+        }
+      },
+      eventSessions: {
+        url: '/sessions',
+        views: {
+          'sessions-tab': {
+            templateUrl: 'js/events/event-sessions.html',
+            controller: 'EventSessionsCtrl'
+          }
+        }
+      },
+      eventSession: {
+        url: '/sessions/:sessionId',
+        views: {
+          'sessions-tab': {
+            templateUrl: 'js/events/event-session.html',
+            controller: 'EventSessionCtrl'
+          }
         },
-        userData: function($stateParams, EventSrv, IonicUtils){
-          return IonicUtils.withLoading(EventSrv.getUserData($stateParams.eventId));
+        resolve: {
+          session: function($stateParams, EventSrv, IonicUtils){
+            return IonicUtils.withLoading(EventSrv.getSession($stateParams.eventId, $stateParams.sessionId));
+          }
+        }
+      },
+      eventExponents: {
+        url: '/exponents',
+        views: {
+          'exponents-tab': {
+            templateUrl: 'js/events/event-exponents.html',
+            controller: 'EventExponentsCtrl'
+          }
+        }
+      },
+      eventExponent: {
+        url: '/exponents/:exponentId',
+        views: {
+          'exponents-tab': {
+            templateUrl: 'js/events/event-exponent.html',
+            controller: 'EventExponentCtrl'
+          }
+        },
+        resolve: {
+          exponent: function($stateParams, EventSrv, IonicUtils){
+            return IonicUtils.withLoading(EventSrv.getExponent($stateParams.eventId, $stateParams.exponentId));
+          }
+        }
+      },
+      eventProgram: {
+        url: '/program',
+        views: {
+          'program-tab': {
+            templateUrl: 'js/events/event-program.html',
+            controller: 'EventProgramCtrl'
+          }
         }
       }
-    })
-      .state('app.event.infos', {
-      url: '/infos',
-      views: {
-        'infos-tab': {
-          templateUrl: 'js/events/event-infos.html',
-          controller: 'EventInfosCtrl'
-        }
-      }
-    })
-      .state('app.event.sessions', {
-      url: '/sessions',
-      views: {
-        'sessions-tab': {
-          templateUrl: 'js/events/event-sessions.html',
-          controller: 'EventSessionsCtrl'
-        }
-      }
-    })
-      .state('app.event.session', {
+    };
+
+    var sessionDetailRoute = {
       url: '/sessions/:sessionId',
       views: {
-        'sessions-tab': {
+        'program-tab': {
           templateUrl: 'js/events/event-session.html',
           controller: 'EventSessionCtrl'
         }
@@ -61,20 +109,11 @@
           return IonicUtils.withLoading(EventSrv.getSession($stateParams.eventId, $stateParams.sessionId));
         }
       }
-    })
-      .state('app.event.exponents', {
-      url: '/exponents',
-      views: {
-        'exponents-tab': {
-          templateUrl: 'js/events/event-exponents.html',
-          controller: 'EventExponentsCtrl'
-        }
-      }
-    })
-      .state('app.event.exponent', {
+    };
+    var exponentDetailRoute = {
       url: '/exponents/:exponentId',
       views: {
-        'exponents-tab': {
+        'program-tab': {
           templateUrl: 'js/events/event-exponent.html',
           controller: 'EventExponentCtrl'
         }
@@ -84,15 +123,18 @@
           return IonicUtils.withLoading(EventSrv.getExponent($stateParams.eventId, $stateParams.exponentId));
         }
       }
-    })
-      .state('app.event.program', {
-      url: '/program',
-      views: {
-        'program-tab': {
-          templateUrl: 'js/events/event-program.html',
-          controller: 'EventProgramCtrl'
-        }
-      }
-    });
+    };
+
+    $stateProvider
+      .state('app.events', routes.events)
+      .state('app.event', routes.event)
+      .state('app.event.infos', routes.eventInfo)
+      .state('app.event.sessions', routes.eventSessions)
+      .state('app.event.session', routes.eventSession)
+      .state('app.event.exponents', routes.eventExponents)
+      .state('app.event.exponent', routes.eventExponent)
+      .state('app.event.program', routes.eventProgram)
+      .state('app.event.programSession', sessionDetailRoute)
+      .state('app.event.programExponent', exponentDetailRoute);
   }
 })();
