2942b14dd4b585390aed8cacb4c6a9e49bbda6b3
Loïc Knuchel <loicknuchel@gmail.com>
2015-05-25 09:03:25 +0200

    improve list of events

--- www/js/events/events.ctrl.js
+++ www/js/events/events.ctrl.js
@@ -3,18 +3,24 @@
   angular.module('app')
     .controller('EventsCtrl', EventsCtrl);
 
-  function EventsCtrl($scope, EventSrv, events){
+  function EventsCtrl($scope, $window, EventSrv, events){
     var vm = {};
     $scope.vm = vm;
 
     vm.events = events;
+
     vm.doRefresh = doRefresh;
+    vm.mapUrl = mapUrl;
 
     function doRefresh(){
       EventSrv.refreshEventList().then(function(events){
         vm.events = events;
         $scope.$broadcast('scroll.refreshComplete');
       });
+    }
+    function mapUrl(address, height){
+      if(!height){ height = 250; }
+      return 'https://maps.googleapis.com/maps/api/staticmap?markers=color:red%7C'+address+'&zoom=15&size='+($window.innerWidth-20)+'x'+height;
     }
   }
 })();
