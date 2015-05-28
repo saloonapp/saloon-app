bb162fc849a0983de371615ecd0f98ac479410f8
Quentin Valmori <quentin.valmori@gmail.com>
2015-05-28 10:42:11 +0200

    iOS8 workaround for non-64 bits devices. Apple bug...

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,7 @@
   'use strict';
   angular.module('app', ['ionic', 'ngCordova', 'LocalForageModule', 'btford.markdown', 'monospaced.elastic'])
     .config(configure)
+    .config(fixiOS8)
     .run(runBlock);
 
   configure.$inject = ['$urlRouterProvider', '$provide'];
@@ -11,6 +12,81 @@
     // improve angular logger
     $provide.decorator('$log', ['$delegate', 'customLogger', function($delegate, customLogger){
       return customLogger($delegate);
+    }]);
+  }
+
+//https://github.com/angular/angular.js/issues/9128
+  fixiOS8.$inject = ['$provide'];
+  function fixiOS8($provide){
+    // Minification-safe hack.
+    var $$watchers = '$$watchers',
+        $$nextSibling = '$$nextSibling',
+        $$childHead = '$$childHead',
+        $$childTail = '$$childTail',
+        $$listeners = '$$listeners',
+        $$listenerCount = '$$listenerCount',
+        $$ChildScope = '$$childScope',
+        $id = '$id',
+        $parent = '$parent',
+        $$prevSibling = '$$prevSibling',
+        $root = '$root';
+
+    $provide.decorator('$rootScope', ['$delegate', function($rootScope) {
+      var proto = Object.getPrototypeOf($rootScope);
+
+      function nextUid () {
+        return ++$rootScope.$id;
+      }
+
+      proto.$new = function(isolate, parent) {
+        var child;
+
+        function destroyChild() {
+          child.$$destroyed = true;
+        }
+
+        parent = parent || this;
+
+        if (isolate) {
+          child = new proto.constructor();
+          child[$root] = this.$root;
+        } else {
+          // Only create a child scope class if somebody asks for one,
+          // but cache it to allow the VM to optimize lookups.
+          if (!this.$$ChildScope) {
+            this[$$ChildScope] = function ChildScope() {
+              this[$$watchers] = this[$$nextSibling] =
+                  this[$$childHead] = this[$$childTail] = null;
+              this[$$listeners] = {};
+              this[$$listenerCount] = {};
+              this[$id] = nextUid();
+              this[$$ChildScope] = null;
+            };
+            this[$$ChildScope].prototype = this;
+          }
+          child = new this[$$ChildScope]();
+        }
+
+        child[$parent] = parent;
+        child[$$prevSibling] = parent.$$childTail;
+        if (parent.$$childHead) {
+          parent[$$childTail][$$nextSibling] = child;
+          parent[$$childTail] = child;
+        } else {
+          parent[$$childHead] = parent[$$childTail] = child;
+        }
+
+        // When the new scope is not isolated or we inherit from `this`, and
+        // the parent scope is destroyed, the property `$$destroyed` is inherited
+        // prototypically. In all other cases, this property needs to be set
+        // when the parent scope is destroyed.
+        // The listener needs to be added after the parent is set
+        if (isolate || parent != this) child.$on('$destroy', destroyChild);
+
+        return child;
+      };
+      $rootScope.$new = proto.$new;
+      return $rootScope;
     }]);
   }
 
