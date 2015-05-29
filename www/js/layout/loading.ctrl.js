af51f42bd4e86237f53cca1acea13a8138213bfa
Loïc Knuchel <loicknuchel@gmail.com>
2015-05-29 19:53:20 +0200

    try to identify user...

--- www/js/layout/loading.ctrl.js
+++ www/js/layout/loading.ctrl.js
@@ -3,8 +3,8 @@
   angular.module('app')
     .controller('LoadingCtrl', LoadingCtrl);
 
-  LoadingCtrl.$inject = ['$scope', '$state', '$q', '$timeout', '$ionicHistory', 'UserSrv', 'EventSrv'];
-  function LoadingCtrl($scope, $state, $q, $timeout, $ionicHistory, UserSrv, EventSrv){
+  LoadingCtrl.$inject = ['$scope', '$state', '$q', '$timeout', '$ionicHistory', '$analytics', 'UserSrv', 'EventSrv'];
+  function LoadingCtrl($scope, $state, $q, $timeout, $ionicHistory, $analytics, UserSrv, EventSrv){
     var vm = {};
     $scope.vm = vm;
 
@@ -19,6 +19,11 @@
     function redirect(){
       $timeout(function(){
         $q.all([UserSrv.getUser(), EventSrv.getAll()]).then(function(results){
+          var user = results[0];
+          var props = {};
+          if(user && user.uuid){ props.userId = user.uuid; }
+          if(user && user.device && user.device.uuid){ props.deviceId = user.device.uuid; }
+          $analytics.setUserProperties(props); // TODO : seems to not work...
           $ionicHistory.nextViewOptions({
             disableAnimate: true,
             disableBack: true,
