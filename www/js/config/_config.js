4ca5cfc6f835d41384d82737e8a6f600a272259b
Loïc Knuchel <loicknuchel@gmail.com>
2015-05-31 00:26:06 +0200

    format for build + app build script for android

--- www/js/config/_config.js
+++ www/js/config/_config.js
@@ -1,17 +1,37 @@
 var Config = (function(){
   'use strict';
-  var cfg = {
-    appVersion: '~',
-    debug: true, // to toggle features between dev & prod
-    verbose: true, // should log in console more infos
-    track: false, // should send tracking events & crashs to the server
-    storage: true, // should save data to browser storage
-    storagePrefix: 'saloon-', // prefix all stoarge entries with this prefix
+  var dev = {
+    debug: true,
+    verbose: true,
+    track: false,
     backendUrl: 'https://dev-saloon.herokuapp.com/api/v1',
     //backendUrl: 'http://localhost:9000/api/v1',
-    emailSupport: 'contact@saloonapp.co',
+    segmentio: {
+      writeKey: ''
+    }
+  };
+  var prod = {
+    debug: false,
+    verbose: false,
+    track: true,
+    backendUrl: 'https://saloonapp.herokuapp.com/api/v1',
     segmentio: {
       writeKey: 'G5zTzGBygajCfZoGb5xt2dd2djRI70mW'
+    }
+  };
+  var env = dev;
+
+  var cfg = {
+    appVersion: '1.0.0~',
+    debug: env.debug, // to toggle features between dev & prod
+    verbose: env.verbose, // should log in console more infos
+    track: env.track, // should send tracking events & crashs to the server
+    storage: true, // should save data to browser storage
+    storagePrefix: 'saloon-', // prefix all stoarge entries with this prefix
+    backendUrl: env.backendUrl,
+    emailSupport: 'contact@saloonapp.co',
+    segmentio: {
+      writeKey: env.segmentio.writeKey
     }
   };
   return cfg;
