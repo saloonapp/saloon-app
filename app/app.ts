b01ab46c7bc451830c8863d390a85a9c62cde281
Loïc Knuchel <loicknuchel@gmail.com>
2016-03-28 18:22:14 +0200

    set french locale to moment

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,7 @@
 import {App, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
+import * as moment from "moment";
+import * as fr from "moment/locale/fr"
 import {TabsPage} from './pages/tabs/tabs';
 import {PluginUtils} from "./common/plugins/utils";
 import {SQLitePlugin} from "./common/plugins/sqlite.plugin";
@@ -12,18 +14,19 @@ import {TfidfService} from "./common/tfidf.service";
 import {EventListPage} from "./pages/event-list.page";
 
 @App({
-  template: '<ion-nav [root]="rootPage"></ion-nav>',
-  providers: [PluginUtils, SQLitePlugin, SQLiteStorage, StorageUtils, Storage, Backend, EventService, TfidfService],
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+    template: '<ion-nav [root]="rootPage"></ion-nav>',
+    providers: [PluginUtils, SQLitePlugin, SQLiteStorage, StorageUtils, Storage, Backend, EventService, TfidfService],
+    config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 export class MyApp {
-  rootPage: any = EventListPage;
+    rootPage: any = EventListPage;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-    });
-  }
+    constructor(platform: Platform) {
+        moment.locale('fr', fr);
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+        });
+    }
 }
