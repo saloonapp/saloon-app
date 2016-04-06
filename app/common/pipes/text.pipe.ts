80946732cd8ba8aa13520807155b47197182e1b6
Loïc Knuchel <loicknuchel@gmail.com>
2016-04-06 20:04:02 +0200

    fix bug

--- app/common/pipes/text.pipe.ts
+++ app/common/pipes/text.pipe.ts
@@ -3,6 +3,6 @@ import {Pipe, PipeTransform} from "angular2/core";
 @Pipe({name: 'capitalize'})
 export class CapitalizePipe implements PipeTransform {
     transform(text: string): string {
-        return text.charAt(0).toUpperCase() + text.slice(1);
+        return text ? text.charAt(0).toUpperCase() + text.slice(1) : text;
     }
 }
