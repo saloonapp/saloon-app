0fca3351b714624f3a5dbeea09b343a87dfeb6c0
Loïc Knuchel <loicknuchel@gmail.com>
2016-05-03 11:06:09 +0200

    segment event list to past & future

--- app/common/utils/date.ts
+++ app/common/utils/date.ts
@@ -2,6 +2,12 @@ import * as moment from "moment";
 import {ObjectHelper} from "./object";
 
 export class DateHelper {
+    public static milli: number = 1;
+    public static sec: number = DateHelper.milli*1000;
+    public static min: number = DateHelper.sec*60;
+    public static hour: number = DateHelper.min*60;
+    public static day: number = DateHelper.hour*24;
+
     public static now(): number {
         //return new Date("Wed Apr 20 2016 09:00:39 GMT+0200 (CEST)").getTime();
         return Date.now();
--- app/event/event-list.page.ts
+++ app/event/event-list.page.ts
@@ -4,8 +4,10 @@ import {NavController} from "ionic-angular/index";
 import {EventItem} from "./models/Event";
 import {EventItemComponent} from "./components/event-item.component";
 import {EventService} from "./services/event.service";
+import {DateHelper} from "../common/utils/date";
 import {UiHelper} from "../common/ui/utils";
 import {EventPage} from "./event.page.ts";
+import {Config} from "../config";
 
 @Page({
     directives: [EventItemComponent],
@@ -13,18 +15,34 @@ import {EventPage} from "./event.page.ts";
 <ion-navbar *navbar>
     <ion-title>Événements</ion-title>
 </ion-navbar>
+<ion-toolbar>
+    <ion-segment [(ngModel)]="segmentValue" (change)="updateSegment()">
+        <ion-segment-button *ngFor="#s of segmentValues" value="{{s.value}}">{{s.label}}</ion-segment-button>
+    </ion-segment>
+</ion-toolbar>
 <ion-content class="event-list-page">
     <ion-refresher (refresh)="doRefresh($event)"><ion-refresher-content></ion-refresher-content></ion-refresher>
-    <event-item *ngFor="#event of events" [event]="event" (click)="goToEvent(event)"></event-item>
-    <p padding>
-        Si votre événement n'est pas référencé ici, nous pouvons l'ajouter.
-        Pour ça, il vous suffit de nous le signaler.
-    </p>
+    <div *ngIf="!events" style="text-align: center; margin-top: 100px;"><ion-spinner></ion-spinner></div>
+    <event-item *ngFor="#event of showedEvents" [event]="event" (click)="goToEvent(event)"></event-item>
+    <div *ngIf="segmentValue === 'future'" padding>
+        <h1 *ngIf="showedEvents && showedEvents.length === 0">Aucun événement à venir :(</h1>
+        <p>
+            Si votre événement n'est pas référencé ici, nous pouvons l'ajouter.<br>
+            Pour ça, il vous suffit de <a href="mailto:{{emailSupport}}?subject=[SalooN] Ajout d'événement&body=Salut, pouvez-vous ajouter l'événement ... (nom, date, lieu...)" target="_blank">nous l'indiquer</a>.
+        </p>
+    </div>
 </ion-content>
 `
 })
 export class EventListPage implements OnInit {
+    segmentValues = [
+        {value: 'future', label: 'À venir', filter: (e: EventItem) => (e.end || e.start)+(1*DateHelper.day) > DateHelper.now()},
+        {value: 'past',   label: 'Passées', filter: (e: EventItem) => (e.end || e.start)+(1*DateHelper.day) < DateHelper.now()}
+    ];
+    segmentValue: string = this.segmentValues[0].value;
     events: EventItem[];
+    showedEvents: EventItem[];
+    emailSupport: string = Config.emailSupport;
     constructor(private _nav: NavController,
                 private _eventService: EventService,
                 private _uiHelper: UiHelper) {}
@@ -32,7 +50,7 @@ export class EventListPage implements OnInit {
     // TODO http://ionicframework.com/docs/v2/api/components/virtual-scroll/VirtualScroll/
     ngOnInit() {
         this._eventService.getEvents().then(
-            events => this.events = events,
+            events => this.updateEvents(events),
             error => this._uiHelper.alert(this._nav, 'Fail to update :(')
         );
     }
@@ -40,7 +58,7 @@ export class EventListPage implements OnInit {
     doRefresh(refresher) {
         this._eventService.fetchEvents().then(
             events => {
-                this.events = events;
+                this.updateEvents(events);
                 refresher.complete();
             },
             error => {
@@ -48,6 +66,16 @@ export class EventListPage implements OnInit {
                 refresher.complete();
             }
         );
+    }
+
+    updateEvents(events: EventItem[]) {
+        this.events = events;
+        this.updateSegment();
+    }
+
+    updateSegment() {
+        const segment = this.segmentValues.find(s => s.value === this.segmentValue);
+        this.showedEvents = segment && this.events ? this.events.filter(segment.filter) : this.events;
     }
 
     goToEvent(eventItem: EventItem) {
