26ea16642aae4bc1354646a4ce8f7d320d4512ba
Loïc Knuchel <loicknuchel@gmail.com>
2016-05-04 18:45:30 +0200

    improve event list

--- app/event/components/event-item.component.ts
+++ app/event/components/event-item.component.ts
@@ -6,22 +6,23 @@ import {DatePeriodPipe} from "../../common/pipes/datetime.pipe";
     selector: 'event-item',
     pipes: [DatePeriodPipe],
     template: `
-<ion-card>
+<ion-card *ngIf="format === 'card'">
     <img [src]="event.landing">
     <ion-card-content>
         <ion-card-title>{{event.name}}</ion-card-title>
         <h3>{{[event.start | datePeriod:event.end, event.address.city].filter(notEmpty).join(', ')}}</h3>
     </ion-card-content>
 </ion-card>
-<!--<ion-item>
-    <ion-avatar item-left><ion-img [src]="event.logo"></ion-img></ion-avatar>
+<ion-item *ngIf="format !== 'card'">
+    <ion-avatar item-left><img [src]="event.logo"></ion-avatar>
     <h2>{{event.name}}</h2>
     <p>{{[event.start | datePeriod:event.end, event.address.city].filter(notEmpty).join(', ')}}</p>
-</ion-item>-->
+</ion-item>
 `
 })
 export class EventItemComponent {
     @Input() event: EventItem;
+    @Input() format: string;
 
     notEmpty(e: string): boolean {
         return e ? e.length > 0 : false;
--- app/event/event-list.page.ts
+++ app/event/event-list.page.ts
@@ -8,6 +8,7 @@ import {DateHelper} from "../common/utils/date";
 import {UiHelper} from "../common/ui/utils";
 import {EventPage} from "./event.page.ts";
 import {Config} from "../config";
+import {Sort} from "../common/utils/array";
 
 @Page({
     directives: [EventItemComponent],
@@ -23,7 +24,7 @@ import {Config} from "../config";
 <ion-content>
     <ion-refresher (refresh)="doRefresh($event)"><ion-refresher-content></ion-refresher-content></ion-refresher>
     <div *ngIf="!events" style="text-align: center; margin-top: 100px;"><ion-spinner></ion-spinner></div>
-    <event-item *ngFor="#event of showedEvents" [event]="event" (click)="goToEvent(event)"></event-item>
+    <event-item *ngFor="#event of showedEvents" [event]="event" [format]="eventFormat" (click)="goToEvent(event)"></event-item>
     <div *ngIf="segmentValue === 'future'" padding>
         <h1 *ngIf="showedEvents && showedEvents.length === 0">Aucun événement à venir :(</h1>
         <p>
@@ -36,10 +37,11 @@ import {Config} from "../config";
 })
 export class EventListPage implements OnInit {
     segmentValues = [
-        {value: 'future', label: 'À venir', filter: (e: EventItem) => (e.end || e.start)+(1*DateHelper.day) > DateHelper.now()},
-        {value: 'past',   label: 'Passées', filter: (e: EventItem) => (e.end || e.start)+(1*DateHelper.day) < DateHelper.now()}
+        {value: 'future', label: 'À venir', format: 'card', filter: (e: EventItem) => (e.end || e.start)+(1*DateHelper.day) > DateHelper.now(), sort: (e1: EventItem, e2: EventItem) => Sort.num(e1.start, e2.start)},
+        {value: 'past',   label: 'Passées', format: 'item', filter: (e: EventItem) => (e.end || e.start)+(1*DateHelper.day) < DateHelper.now(), sort: (e1: EventItem, e2: EventItem) => -Sort.num(e1.start, e2.start)}
     ];
     segmentValue: string = this.segmentValues[0].value;
+    eventFormat: string = this.segmentValues[0].format;
     events: EventItem[];
     showedEvents: EventItem[];
     emailSupport: string = Config.emailSupport;
@@ -47,7 +49,6 @@ export class EventListPage implements OnInit {
                 private _eventService: EventService,
                 private _uiHelper: UiHelper) {}
 
-    // TODO http://ionicframework.com/docs/v2/api/components/virtual-scroll/VirtualScroll/
     ngOnInit() {
         this._eventService.getEvents().then(
             events => this.updateEvents(events),
@@ -75,7 +76,8 @@ export class EventListPage implements OnInit {
 
     updateSegment() {
         const segment = this.segmentValues.find(s => s.value === this.segmentValue);
-        this.showedEvents = segment && this.events ? this.events.filter(segment.filter) : this.events;
+        this.eventFormat = segment ? segment.format : this.segmentValues[0].format;
+        this.showedEvents = segment && this.events ? this.events.filter(segment.filter).sort(segment.sort) : this.events;
     }
 
     goToEvent(eventItem: EventItem) {
