81359e2f5f8f9ee52c16b00f13fb0a8f3304eae1
Loïc Knuchel <loicknuchel@gmail.com>
2016-05-09 21:48:20 +0200

    build version 2.0.0

--- app/config.ts
+++ app/config.ts
@@ -7,10 +7,10 @@ export class Config {
     };
     private static env = Config.dev;
 
-    public static appVersion = '2.0.0~';
+    public static appVersion = '2.0.0';
     public static gitBranch = 'v2';
-    public static gitCommit = '5dc78ce';
-    public static buildDate = '2016-05-05 07:09';
+    public static gitCommit = '7c3989d';
+    public static buildDate = '2016-05-09 21:46';
     public static emailSupport = 'contact@saloonapp.co';
     public static backendUrl = Config.env.backendUrl;
 }
